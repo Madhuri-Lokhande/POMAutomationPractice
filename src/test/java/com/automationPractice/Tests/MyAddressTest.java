9818f519bcec00dcc968fdc28d871c0edc3ed931
Madhuri-Lokhande <lokhande8madhuri@email.com>
2021-04-05 16:39:37 +0100

    Added Extent

--- src/test/java/com/automationPractice/Tests/MyAddressTest.java
+++ src/test/java/com/automationPractice/Tests/MyAddressTest.java
@@ -44,7 +44,8 @@ public class MyAddressTest extends TestClass {
 		System.out.println("URL of My Address is: "+myaddrs.getPageUrl());
 	}
 	
-	@Test(priority = 2)
+	@Ignore
+	//Test(priority = 2)
 	public void checkMyAddressasd() {
 	//Assert.assertTrue(myaddrs.addNewAddressButton());
 		//Assert.assertEquals(myaddrs.addNewAddressButton(),"My addressasd");
